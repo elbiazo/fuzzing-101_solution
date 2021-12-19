9f69674a442a205f8c39d8ecdf962d461d0b87af
elbiazo <eric.l.biazo@gmail.com>
2021-12-19 02:17:55 -0500

    adding commen to learn how to run

--- exercise-3/src/main.rs
+++ exercise-3/src/main.rs
@@ -5,6 +5,7 @@ pub const MAP_SIZE: usize = 80642;
 fn main() {
     let parsed_opts = parser::parse_args();
 
+    // ./build/exercise-3 -i corpus/ -o solutions/ -c 0-7 -t ./build/sbin/tcpdump --args -vr @@
     ForkserverBytesCoverageSugar::<MAP_SIZE>::builder()
         .input_dirs(&[parsed_opts.input])
         .output_dir(parsed_opts.output)
